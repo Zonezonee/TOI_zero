226c40b8c12b0d1aa1a4dc129fe729227b8e9bf5
zone_zonee <rubsarbnaja@gmail.com>
2025-04-23 23:20:12 +0700

    Added A3-004 solution.

--- A3/A3-004.cpp
+++ A3/A3-004.cpp
@@ -1,34 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+
     int n;
     cin >> n;
-    pair<int, int> a[n];
-    int x[n], y[n];
-    // for(auto &[x, y] : a) cin >> x >> y;
-    for(int i = 0; i < n; ++i) cin >> x[i] >> y[i];
-    // sort(a, a+n);
-    sort(x, x+n);
-    sort(y, y+n);
-    // for(auto &[x, y] : a) cout << x << ' ' << y << '\n';
-    pair<float, float> median;
-    if(n&1){
-        median = {x[n/2], y[n/2]};
-    }else{
-        median = {(x[n/2]+x[n/2-1])/2.0, (x[n/2]+y[n/2-1])/2.0};
-    }
-    // median = a[n/2];
-    // cout << median.first << ' ' << median.second << '\n';
-    int dif = median.first - median.second;
-    long long res = 0;
-    for(int i = 0; i < n; ++i){
-        res += abs(dif - (x[i]-y[i]));
-        x[i] += (dif - (x[i]-y[i]))/2;
+    vector<int> u(n), v(n);
 
-        res += abs(median.first - x[i])*2;
+    for (int i = 0; i < n; ++i) {
+        int x, y;
+        cin >> x >> y;
+        u[i] = x + y;
+        v[i] = x - y;
     }
+
+    // Find medians of u and v
+    sort(u.begin(), u.end());
+    sort(v.begin(), v.end());
+    int median_u = u[n / 2];
+    int median_v = v[n / 2];
+
+    long long res = 0;
+    for (int i = 0; i < n; ++i) {
+        res += abs(u[i] - median_u) + abs(v[i] - median_v);
+    }
+
     cout << res << '\n';
+    return 0;
 }
